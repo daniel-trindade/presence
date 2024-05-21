a000802e40ac8bf3fb479d3c2f52cf1a70bb403e
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-05-21 00:36:05 -0300

    Login page finished

--- src/Components/pages/Login.jsx
+++ src/Components/pages/Login.jsx
@@ -1,9 +1,18 @@
 import styles from "./Login.module.css"
 
+import Button from "../layout/Button"
+
 function Login(){
   return(
-    <div>
-      <h1>Login Page</h1>
+    <div className={styles.loginContainer}>
+      <form className={styles.loginForm}>
+        <h1>Login</h1>
+        <input type="text" className={styles.loginBox} id="login"/>
+        <h1>Senha</h1>
+        <input type="password" className={styles.loginBox} id="password"/>
+        <input type="submit" value="Entrar" className={styles.enterButton}/>
+      </form>
+      
     </div>
   )
 }
