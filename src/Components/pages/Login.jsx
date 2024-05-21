aaa147aa400b85548a211cc75a726fc17c3d29b7
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-05-21 00:58:15 -0300

    Update Login.jsx

--- src/Components/pages/Login.jsx
+++ src/Components/pages/Login.jsx
@@ -3,6 +3,8 @@ import styles from "./Login.module.css"
 import Button from "../layout/Button"
 
 function Login(){
+
+  
   return(
     <div className={styles.loginContainer}>
       <form className={styles.loginForm}>
