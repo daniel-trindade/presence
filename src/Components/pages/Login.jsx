5226e99e1ddd3e5398b563d29d7ac431f3edefca
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-05-21 17:16:06 -0300

    Login page finished

--- src/Components/pages/Login.jsx
+++ src/Components/pages/Login.jsx
@@ -1,17 +1,36 @@
 import styles from "./Login.module.css"
+import { useState } from "react"
+import { FaUser, FaLock } from "react-icons/fa"
 
 import Button from "../layout/Button"
 
 function Login(){
 
+  const [userName, setUserName] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    alert("Enviando os dados: " + userName +" - " + password)
+  }
   
   return(
     <div className={styles.loginContainer}>
-      <form className={styles.loginForm}>
-        <h1>Login</h1>
-        <input type="text" className={styles.loginBox} id="login"/>
-        <h1>Senha</h1>
-        <input type="password" className={styles.loginBox} id="password"/>
+      <form onSubmit={handleSubmit} className={styles.loginForm}>
+        <h1>Login <span><FaUser/></span></h1>
+        <input 
+          type="text" 
+          className={styles.loginBox} 
+          id="login" 
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <h1>Senha <span><FaLock/></span></h1>
+        <input 
+          type="password" 
+          className={styles.loginBox} 
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <input type="submit" value="Entrar" className={styles.enterButton}/>
       </form>
       
