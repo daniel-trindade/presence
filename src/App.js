551fec3f6ba22a6080a7225c189e83234901e143
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-05-29 23:27:36 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
   return (
     <>
       <NavBar/>
-      <Container>
-        <Paths/>
-      </Container>
+        <Container>
+          <Paths/>
+        </Container>
       <Footer/>
     </>
   );
