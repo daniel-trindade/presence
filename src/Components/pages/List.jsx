50bb3e1ca07cafae8966caf4bfd0b637611bae2c
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-06-17 19:37:10 -0300

    Update List.jsx

--- src/Components/pages/List.jsx
+++ src/Components/pages/List.jsx
@@ -5,6 +5,9 @@ function List(){
   return(
     <div className={styles.listContainer}>
       <h1>List</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, maxime repellendus blanditiis quia expedita molestias cum commodi eaque accusamus quo, doloremque totam odio eius eos amet dicta non cumque rerum.
+      </p>
     </div>
   )
 
