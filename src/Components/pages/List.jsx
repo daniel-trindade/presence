07b86e137bcafab1c967538319002ae6af2fd0ed
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-07-21 23:51:32 -0300

    lista aparecendo

--- src/Components/pages/List.jsx
+++ src/Components/pages/List.jsx
@@ -1,35 +1,41 @@
-import { useState } from "react"
+import { useState, useEffect} from "react"
 import { app } from "../../firebaseConfig"
-import { getDatabase, ref, get } from "firebase/database"
+import { getDatabase, ref, onValue} from "firebase/database"
 
 import styles from "./List.module.css"
 
 function List(){
 
-  let [membersList, setMenberist] = useState([])
+  let [membersList, setMenberList] = useState([])
 
   const fechData = async () => {
     const db = getDatabase(app);
-    const dbRef = ref(db, "presence")
-    const snapshot = await get(dbRef)
-    if(snapshot){
-      setMenberist(Object.values(snapshot.val()))
-    }else{
-      alert("error")
-    }
+    const dbRef = ref(db, 'Presence/');
+    onValue(dbRef, (snapshot) => {
+      const data = snapshot.val();
+      setMenberList(data)
+    });
   }
+
+
 
   return(
     <div className={styles.listContainer}>
       <h1>List</h1>
       <button onClick={fechData}>Lançar Frequencia</button>
-      <ul>
-        {membersList.map( (member, index) =>(
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore officia, eum iste neque, ducimus modi vel culpa animi, architecto reprehenderit sint nam ut. Illo praesentium esse veritatis ullam, excepturi earum.</p>
+     
+
+
+      {/* <ul>
+        {membersList.map((member, index) =>(
           <li key={index}>
             {member.id} - {member.name}
           </li>
         ) )}
-      </ul>
+      </ul> */}
+      
+
     </div>
   )
 
