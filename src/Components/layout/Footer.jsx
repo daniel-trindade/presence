25b1ca369fa9e3fcef332a1a0ca3f61c1d7fbaed
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-07-26 01:03:44 -0300

    Update Footer.jsx

--- src/Components/layout/Footer.jsx
+++ src/Components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css"
 function Footer(){
   return(
     <div className={styles.footer}>
-      <a href="https://portfolio-kappa-lake-89.vercel.app/">Trindade &copy; 2024</a> 
+      <a href="https://portfolio-kappa-lake-89.vercel.app/" target="blank">Trindade &copy; 2024</a> 
     </div>
   )
 }
